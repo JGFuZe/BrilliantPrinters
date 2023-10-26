84ee8d5ce978c245fb582814a9297797479c44f0
JGFuZe <minemania36@gmail.com>
2023-10-26 13:51:32 -0600

    working question class

--- BrilliantPrinters_app/models.py
+++ BrilliantPrinters_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    description = models.CharField(max_length=200, blank=False)
+    answered = models.BooleanField(default=False)
