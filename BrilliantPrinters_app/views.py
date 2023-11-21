bbe208de98e72106e694894b56874e48d9357f99
JGFuZe <minemania36@gmail.com>
2023-11-20 19:54:10 -0700

    after

--- BrilliantPrinters_app/views.py
+++ BrilliantPrinters_app/views.py
@@ -79,30 +79,47 @@ def profile(request):
 #@login_required(login_url='login')
 #@allowed_users(allowed_roles=['regular_user'])
 def createQuestion(request):
-    form = QuestionForm()
+    questionForm = QuestionForm()
     fileForm = FileForm()
-
 
     user = User.objects.get(id=request.user.id)
     respondent = Respondent.objects.get(user=user)
     respondent_id = respondent.id
 
-    if request.method == 'POST':
+    #
+    if ('questionSubmit' in request.POST):
+        questionData = request.POST.copy()
+        questionData['respondent_id'] = respondent_id
 
-        question_data = request.POST.copy()
-        question_data['respondent_id'] = respondent_id
-
-        form = QuestionForm(question_data)
-        if form.is_valid():
-            question = form.save()  # Save the form
+        #
+        questionForm = QuestionForm(questionData, prefix='')
+        
+        if questionForm.is_valid():
+            question = questionForm.save()  # Save the form
             question.respondent = respondent
             question.save()         # Save question
             print(question.respondent)
 
             # Redirect back to portfolio details page
             return redirect('question_list')
+
+    elif ('fileSubmit' in request.POST):
+        fileData = request.POST.copy()
+        fileForm = FileForm(fileData, prefix='')
+
+        if (fileForm.is_valid()):
+            file = fileForm.save()  # save form data object
+            file.save()         #
+
+            return redirect('question_list')
+
+    else:
+        questionForm = QuestionForm(prefix='')
+        fileForm = FileForm(prefix='')
+
+    
         
-    context = {'form':form}
+    context = {'questionForm':questionForm, 'fileSubmitForm':fileForm}
     return render(request, 'BrilliantPrinters_app/question_form.html', context)
 
 #
