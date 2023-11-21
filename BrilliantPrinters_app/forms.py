5c41c60cbe963abe62c747753ae4e3610ce83587
JGFuZe <minemania36@gmail.com>
2023-11-20 20:18:57 -0700

    update

--- BrilliantPrinters_app/forms.py
+++ BrilliantPrinters_app/forms.py
@@ -7,16 +7,6 @@ from django.contrib.auth.models import User
 
 #create class for project form
 
-class FileForm(ModelForm):
-    file = forms.FileField(widget = forms.TextInput(attrs={
-        "name": "images",
-        "type": "File",
-        "class": "form-control",
-        "multiple": "True",
-    }), label = "")
-    class Meta:
-        model = TextFile
-        fields = ['file']
 
 
 class QuestionForm(ModelForm):
